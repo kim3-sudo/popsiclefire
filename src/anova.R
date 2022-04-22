cd8ddc746809b051abc8c13d7d9e96f1a9cbc2fe
Sejin Kim <kim3@kenyon.edu>
2022-04-22 14:31:20 -0400

    add start of analysis

---
+++ src/anova.R
@@ -0,0 +1,19 @@
+# anova.R
+# E. Croffoot-Suede, S. Kim
+# Popsicle Fire
+# ANOVA
+
+fire <- read.csv(url("https://raw.githubusercontent.com/kim3-sudo/popsiclefire/main/data/data_filled.csv"))
+
+# Check conditions
+plot(aov(distance ~ type * run, data = fire), which = 1:2) # only to get residual plots
+plot(aov(distance ~ type * run, data = fire), which = 1:2) # only to get residual plots
+
+# Check full model ANOVA
+fire.mod.full <- aov(distance ~ type * run, data = fire)
+fire.mod.null <- aov(distance ~ 1, data = fire)
+anova(fire.mod.null, fire.mod.full)
+plot(fire.mod.full, which = 1:2)
+
+# Friedman's test!
+friedman.test(fire$distance, fire$type, fire$run)
