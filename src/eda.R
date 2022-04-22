2bcb1eb1371ce3f82e1809e55895df8622863da9
Sejin Kim <kim3@kenyon.edu>
2022-04-22 14:07:47 -0400

    Add EDA file

---
+++ src/eda.R
@@ -0,0 +1,6 @@
+# eda.R
+# E. Croffoot-Suede, S. Kim
+# Popsicle Fire
+# Exploratory data analysis
+
+fire <- read.csv(url("https://raw.githubusercontent.com/kim3-sudo/popsiclefire/main/data/data_filled.csv"))
