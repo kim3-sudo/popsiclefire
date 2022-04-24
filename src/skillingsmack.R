4de0433ad0661bf448f33d3986bd97b8514bb9dd
Sejin Kim <kim3@kenyon.edu>
2022-04-24 16:26:12 -0400

    Clean up skillingsmack.R and add interpretation comments

--- src/skillingsmack.R
+++ src/skillingsmack.R
@@ -1,7 +1,7 @@
 # anova.R
 # E. Croffoot-Suede, S. Kim
 # Popsicle Fire
-# ANOVA
+# Skillings-Mack procedure
 
 fire <- read.csv(url("https://raw.githubusercontent.com/kim3-sudo/popsiclefire/main/data/data_filled.csv"))
 
@@ -9,17 +9,10 @@ fire <- read.csv(url("https://raw.githubusercontent.com/kim3-sudo/popsiclefire/m
 plot(aov(distance ~ type * run, data = fire), which = 1:2) # only to get residual plots
 plot(aov(distance ~ type * run, data = fire), which = 1:2) # only to get residual plots
 
-# Check full model ANOVA
-fire.mod.full <- aov(distance ~ type * run, data = fire)
-fire.mod.null <- aov(distance ~ 1, data = fire)
-anova(fire.mod.null, fire.mod.full)
-plot(fire.mod.full, which = 1:2)
-
-# Friedman's test!
+# Skillings-Mack's test!
+# This is an distribution-free test for general alternatives for data from an arbitrary incomplete block design (Hollander - Nonparametric Statistical Methods, 3ed, p. 343)
 library(NSM3)
 pSkilMack(fire$distance, b = as.factor(fire$run), trt = fire$type)
-
-# for shits n giggles
-kruskal.test(fire$distance, fire$type)
-pSDCFlig(fire$distance, g = as.factor(fire$type))
+# A significant p-value here means there is some strong evidence to support the hypothesis that the accelerant used does influence the length of burnt popsicle stick.
+# This does NOT tell us which of the accelerants works the best, only that at least one of them works better than the others.
 
